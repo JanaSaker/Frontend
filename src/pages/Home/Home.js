03442256b20cba579169891500e202ca97a72ace
JanaSaker <janasakr2000@gmail.com>
2023-11-01 15:42:22 +0200

    homepage

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Home.css";
+
+
+const Home = () => {
+  return (
+    <div className="desktop">
+        <div className="overlap-group"> 
+          <img className="image"
+            src="HomePic.png" alt=''/>
+          <div className="rectangle" />
+          <div className="An">An <span>-</span></div>
+          <div className="Towards">Towards</div>
+          <div className="Experience">Experience</div>
+          <div className="Unforgettable">Unforgettable</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
