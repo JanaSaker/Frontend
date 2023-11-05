9bc6ef2f442ddaafbac3e77fb51ce402e1588342
JanaSaker <janasakr2000@gmail.com>
2023-11-05 14:06:35 +0200

    register

---
+++ src/pages/ContactUs/ContactUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="Register"> - Register</div>
+          <div class="box-container">
+            <div class="box1"></div>
+            <div class="box2"></div>
+            <div class="box3"></div>
+            </div>
+          </div>
+        </div> 
+      </div>
+  );
+};
+export default ContactUs
